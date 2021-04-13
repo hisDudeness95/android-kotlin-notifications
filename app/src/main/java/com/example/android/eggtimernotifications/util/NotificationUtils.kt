7099289616c6cfb1666940be5a3091f37c6572ae
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-04-13 12:36:51 -0600

    innit

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -51,6 +51,14 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     )
     // TODO: Step 2.0 add style
 
+    val eggImage = BitmapFactory.decodeResource(
+        applicationContext.resources,
+        R.drawable.cooked_egg
+    )
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(eggImage)
+        .bigLargeIcon(null)
+
     // TODO: Step 2.2 add snooze action
 
     // TODO: Step 1.2 get an instance of NotificationCompat.Builder
@@ -60,7 +68,6 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         applicationContext,
         applicationContext.getString(R.string.egg_notification_channel_id)
     )
-
         // TODO: Step 1.8 use the new 'breakfast' notification channel
 
         // TODO: Step 1.3 set title, text and icon to builder
@@ -78,8 +85,22 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setAutoCancel(true)
     // TODO: Step 2.1 add style to builder
 
-    // TODO: Step 2.3 add snooze action
+        .setStyle(bigPicStyle)
+        .setLargeIcon(eggImage)
+        .addAction(
+            R.drawable.egg_icon,
+            applicationContext.getString(R.string.snooze),
+            snoozePendingIntent
+        )
 
+    // TODO: Step 2.3 add snooze action
+    val snoozeIntent = Intent(applicationContext, SnoozeReceiver::class.java)
+    val snoozePendingIntent: PendingIntent = PendingIntent.getBroadcast(
+        applicationContext,
+        REQUEST_CODE,
+        snoozeIntent,
+        FLAGS
+    )
     // TODO: Step 2.5 set priority
 
     // TODO: Step 1.4 call notify
@@ -89,3 +110,7 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 }
 
 // TODO: Step 1.14 Cancel all notifications
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
+
