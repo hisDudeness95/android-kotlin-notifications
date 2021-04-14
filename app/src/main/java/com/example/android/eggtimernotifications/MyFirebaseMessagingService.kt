e5d01641373b10547832033c8228519f8b397c1e
hisDudeness95 <57687503+hisDudeness95@users.noreply.github.com>
2021-04-14 12:46:35 -0600

    innit

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -1,10 +1,19 @@
 package com.example.android.eggtimernotifications
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseMessagingService {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageRecieved(remoteMessage: RemoteMessage?) {
-        log.d(TAG, "From: ${remoteMessage?.from}")
+        Log.d(TAG, "From: ${remoteMessage?.from}")
+    }
+
+    private override fun onNewToken(token: String?) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        sendRegistrationToServer(token)
     }
 }
